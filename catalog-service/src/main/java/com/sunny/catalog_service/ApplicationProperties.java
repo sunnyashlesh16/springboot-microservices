d139630cff4b1705acf1aac149abe1701a21c31e
sunnyashlesh21 <sunnyashlesh26@gmail.com>
2024-10-11 22:11:13 -0400

    Build spotless issues

--- catalog-service/src/main/java/com/sunny/catalog_service/ApplicationProperties.java
+++ catalog-service/src/main/java/com/sunny/catalog_service/ApplicationProperties.java
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.Min;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
-//Setting the application properties here for the default page.
+// Setting the application properties here for the default page.
 @ConfigurationProperties(prefix = "catalog")
 public record ApplicationProperties(@DefaultValue("10") @Min(1) int pageSize) {}
