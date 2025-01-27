2b4e020983efd152bf3c64967c277bf598ff2b03
saisunny-exergy <sunny@tryinterstellar.com>
2025-01-27 02:22:40 -0500

    Checking catalog Image pushing

--- catalog-service/src/main/java/com/sunny/catalog_service/config/WebMVCConfig.java
+++ catalog-service/src/main/java/com/sunny/catalog_service/config/WebMVCConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+//this is for to allow the catalog service to be accessed from other domains without cors issues
 @Configuration
 class WebMVCConfig implements WebMvcConfigurer {
 
