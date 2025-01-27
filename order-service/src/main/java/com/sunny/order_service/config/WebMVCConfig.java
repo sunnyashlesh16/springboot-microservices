cae6c4fb9659610eb3df07add14eb7ef046b090f
saisunny-exergy <sunny@tryinterstellar.com>
2025-01-27 01:23:36 -0500

    Added Api gateway, Solved CORS Issues, and dockerized

---
+++ order-service/src/main/java/com/sunny/order_service/config/WebMVCConfig.java
@@ -0,0 +1,18 @@
+package com.sunny.order_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false);
+    }
+}
